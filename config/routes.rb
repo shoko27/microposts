1ab12f7d5022a76ca39b9058a658750308423209
shoukosakai <shoko.lily4577@gmail.com>
2016-11-08 04:33:46 +0000

    add posting microposts

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 Rails.application.routes.draw do
   root to: 'static_pages#home'
-end
+  get    'signup', to: 'users#new'
+  get    'login' , to: 'sessions#new'
+  post   'login' , to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
+
+  
+  resources :users
+  resources :microposts
+ end
 
  
